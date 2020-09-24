f4f26254956e503f912beb11b61a2e510ab11f6f
Pratistha Maharjan <pratisthamaharjan@gmail.com>
2020-09-24 10:58:45 -0400

    Initial Commit

---
+++ FlixStar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlixStar
+//
+//  Created by Pratistha Maharjan on 9/24/20.
+//  Copyright © 2020 Pratistha Maharjan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
