4613d2ca53ac493ac18131276353632f43e3fd24
Pratistha Maharjan <pratisthamaharjan@gmail.com>
2020-10-03 19:18:13 -0400

    Done with Navigation and passing data

--- FlixStar/MoviewDetailsViewController.swift
+++ FlixStar/MoviewDetailsViewController.swift
@@ -9,22 +9,25 @@
 import UIKit
 
 class MoviewDetailsViewController: UIViewController {
-
+    var movie: [String : Any]!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        print(movie["title"])
     }
     
-
-    /*
+/*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        //Find the selected cell
+        let cell = sender as! UITableViewController
+        let indexPath = tableView.indexPath (for: cell)!
+        let movie = movies[indexPath.row]
     }
-    */
+*/
 
 }
