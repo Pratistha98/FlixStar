abbe4320a8ffc68a6e8284d5e57f0cd9b96e58c2
Pratistha Maharjan <pratisthamaharjan@gmail.com>
2020-10-03 22:17:03 -0400

    Finished view setup

--- FlixStar/MovieGridViewController.swift
+++ FlixStar/MovieGridViewController.swift
@@ -7,14 +7,17 @@
 //
 
 import UIKit
+import AlamofireImage
+class MovieGridViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
-class MovieGridViewController: UIViewController {
-    
     @IBOutlet var collectionView: UICollectionView!
     
     var movies = [[String:Any]]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -25,14 +28,27 @@ class MovieGridViewController: UIViewController {
            } else if let data = data {
               let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
               self.movies = dataDictionary["results"] as! [[String:Any]]
-            print(self.movies)
+            self.collectionView.reloadData()
            }
         }
         task.resume()
         // Do any additional setup after loading the view.
     }
     
-
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieGridCell", for: indexPath) as! MovieGridCell
+        let movie = movies[indexPath.item]
+        let baseURL = "https://image.tmdb.org/t/p/w185/"
+        let posterPath = movie["poster_path"] as! String
+        let posterURL = URL(string: baseURL + posterPath)
+        cell.posterView.af.setImage(withURL: posterURL!)
+        return cell
+    }
+    
     /*
     // MARK: - Navigation
 
