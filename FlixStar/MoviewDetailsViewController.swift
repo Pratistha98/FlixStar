5137e6b37ee9a9f03bf44a55ef9c5f71bd7260b9
Pratistha Maharjan <pratisthamaharjan@gmail.com>
2020-10-03 20:39:38 -0400

    Added and connected posterview, titleLabel and synopsis label

--- FlixStar/MoviewDetailsViewController.swift
+++ FlixStar/MoviewDetailsViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
-
+import AlamofireImage
 class MoviewDetailsViewController: UIViewController {
+    
+    @IBOutlet weak var backdropView: UIImageView!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
     var movie: [String : Any]!
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(movie["title"])
+        titleLabel.text = movie["title"] as? String
+        synopsisLabel.text = movie["overview"] as? String
+       
+        let baseURL = "https://image.tmdb.org/t/p/w185/"
+        let posterPath = movie["poster_path"] as! String
+        let posterURL = URL(string: baseURL + posterPath)
+        posterView.af.setImage(withURL: posterURL!)
     }
     
 /*
