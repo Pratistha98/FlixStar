52822bb73efa876d6ddbcca8ba19888eaa177cda
Pratistha Maharjan <pratisthamaharjan@gmail.com>
2020-10-03 21:48:14 -0400

    LinkedCollection view cell

---
+++ FlixStar/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  FlixStar
+//
+//  Created by Pratistha Maharjan on 10/3/20.
+//  Copyright © 2020 Pratistha Maharjan. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+}
