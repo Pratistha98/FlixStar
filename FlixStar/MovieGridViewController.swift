1ce889964ff9b258edc06ee0ad6bff0c43fc2a40
Pratistha Maharjan <pratisthamaharjan@gmail.com>
2020-10-03 21:15:34 -0400

    Setup the tab bar controller

---
+++ FlixStar/MovieGridViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MovieGridViewController.swift
+//  FlixStar
+//
+//  Created by Pratistha Maharjan on 10/3/20.
+//  Copyright © 2020 Pratistha Maharjan. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
